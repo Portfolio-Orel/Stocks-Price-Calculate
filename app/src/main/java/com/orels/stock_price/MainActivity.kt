74ecb9b295166927692f8684424c77fe531f58f3
Orelzman <orelsmail@gmail.com>
2023-07-01 16:01:38 +0300

    - Fetch profile and showing it works

--- app/src/main/java/com/orels/stock_price/MainActivity.kt
+++ app/src/main/java/com/orels/stock_price/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.orels.stock_price
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginResult
+import coil.compose.AsyncImage
 import com.facebook.login.widget.LoginButton
 import com.orels.presentation.theme.StockPriceTheme
 import com.orels.presentation.ui.ActivityResultCallbackManager
@@ -76,42 +86,67 @@ fun LoginScreen(viewModel: LoginViewModel = hiltViewModel()) {
         }
     }
     val context = LocalContext.current
-    Column {
-        AndroidView(
-            factory = { context ->
-                // Inflate your XML layout using the LayoutInflater
-                return@AndroidView LayoutInflater.from(context)
-                    .inflate(com.orels.presentation.R.layout.facebook_login_button, null)
-            },
-            update = { view ->
-                // Perform any necessary setup or modifications to the view
-                // For example, you can set listeners or update the view's properties
-
-                // Access specific views within your layout
-                val button =
-                    view.findViewById<LoginButton>(com.orels.presentation.R.id.login_button)
-//                button.setPermissions(listOf("email"))
-                val callbackManager = CallbackManager.Factory.create()
-                button.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
-                    override fun onSuccess(loginResult: LoginResult?) {
-                        println()
+    Column(modifier = Modifier.fillMaxSize()) {
+        if (viewModel.state is LoginViewModel.LoginState.LoggedIn) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.DarkGray)
+                    .padding(50.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically)
+            ) {
+                Icon(Icons.Rounded.Share, contentDescription = "Share", modifier = Modifier.size(40.dp).clickable {
+                    viewModel.share()
+                }, tint = if(viewModel.isShared) Color.Cyan else Color.White)
+                viewModel.friendsList?.forEach {
+                    Text(text = it.name ?: "Hi")
+                }
+                if (viewModel.profileUrl != null) {
+                    AsyncImage(
+                        modifier = Modifier.size(100.dp),
+                        model = viewModel.profileUrl,
+                        contentDescription = null,
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    (0..4).forEach { _ ->
+                        // Star icon
+                        Icon(
+                            imageVector = Icons.Rounded.Star,
+                            contentDescription = "Rating",
+                            tint = Color.Yellow,
+                            modifier = Modifier.size(40.dp)
+                        )
                     }
-
-                    override fun onCancel() {
-                        println()
-                    }
-
-                    override fun onError(exception: FacebookException) {
-                        println()
-                    }
-                })
-                button.setOnClickListener {
-                    viewModel.login(context as? Activity)
                 }
             }
-        )
-        Button(onClick = viewModel::test) {
-            Text(text = "Test")
+            Text(text = "Logged in")
+
+        } else {
+            AndroidView(
+                factory = { context ->
+                    // Inflate your XML layout using the LayoutInflater
+                    return@AndroidView LayoutInflater.from(context)
+                        .inflate(com.orels.presentation.R.layout.facebook_login_button, null)
+                },
+                update = { view ->
+                    // Perform any necessary setup or modifications to the view
+                    // For example, you can set listeners or update the view's properties
+
+                    // Access specific views within your layout
+                    val button =
+                        view.findViewById<LoginButton>(com.orels.presentation.R.id.login_button)
+//                button.setPermissions(listOf("email"))
+
+                    button.setOnClickListener {
+//                    viewModel.login(context as? Activity)
+                    }
+                }
+            )
         }
     }
 
--- presentation/src/main/java/com/orels/presentation/ui/LoginViewModel.kt
+++ presentation/src/main/java/com/orels/presentation/ui/LoginViewModel.kt
@@ -13,6 +13,7 @@ import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.GraphRequest
+import com.facebook.GraphResponse
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.google.firebase.auth.FacebookAuthProvider
@@ -20,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.json.JSONObject
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,22 +29,32 @@ class LoginViewModel @Inject constructor() : ViewModel(), ActivityResultCallback
     sealed class LoginState {
         object Initial : LoginState()
         object Processing : LoginState()
-        data class Success(val loginResult: LoginResult) : LoginState()
+        object LoggedOut : LoginState()
+        object LoggedIn : LoginState()
         data class Error(val exception: FacebookException) : LoginState()
     }
 
     private var auth: FirebaseAuth = Firebase.auth
 
-    private var callbackManager = CallbackManager.Factory.create()
+    var profileUrl by mutableStateOf<String?>(null)
+        private set
+    var friendsList by mutableStateOf<List<Friend>?>(null)
+        private set
+
+    var isShared by mutableStateOf(false)
+        private set
+
+    var callbackManager = CallbackManager.Factory.create()
     var state by mutableStateOf<LoginState>(LoginState.Initial)
         private set
 
     init {
+        checkAuthState()
         LoginManager.getInstance().registerCallback(
             callbackManager,
             object : FacebookCallback<LoginResult> {
                 override fun onSuccess(loginResult: LoginResult) {
-                    state = LoginState.Success(loginResult)
+                    checkAuthState()
                 }
 
                 override fun onCancel() {
@@ -56,6 +68,22 @@ class LoginViewModel @Inject constructor() : ViewModel(), ActivityResultCallback
         )
     }
 
+    fun checkAuthState() {
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+        state = if (isLoggedIn) {
+            getProfileUrl()
+            getFriendsList()
+            LoginState.LoggedIn
+        } else {
+            LoginState.LoggedOut
+        }
+    }
+
+    fun share() {
+        isShared = !isShared
+    }
+
     override fun onCleared() {
         super.onCleared()
         LoginManager.getInstance().unregisterCallback(callbackManager)
@@ -64,31 +92,59 @@ class LoginViewModel @Inject constructor() : ViewModel(), ActivityResultCallback
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean =
         callbackManager.onActivityResult(requestCode, resultCode, data)
 
-    fun login(activity: Activity?) {
-        state = LoginState.Processing
-        activity?.let {
-            LoginManager.getInstance()
-                .logInWithReadPermissions(it, listOf("public_profile"))
+
+    fun getFriendsList() {
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val request = GraphRequest.newGraphPathRequest(
+            accessToken,
+            "/me/friends"
+        ) { response: GraphResponse? ->
+            val jsonObject = response?.jsonObject
+            friendsList = parseFriendList(jsonObject)
         }
+
+        val parameters = Bundle()
+        parameters.putString("fields", "id,name,picture.type(large)")
+        request.parameters = parameters
+        request.executeAsync()
     }
 
-    fun test() {
+    private fun parseFriendList(jsonObject: JSONObject?): List<Friend> {
+        val friendList = mutableListOf<Friend>()
+
+        val friendsData = jsonObject?.optJSONArray("data")
+        if (friendsData != null) {
+            for (i in 0 until friendsData.length()) {
+                val friendObject = friendsData.optJSONObject(i)
+                val id = friendObject?.optString("id")
+                val name = friendObject?.optString("name")
+                val pictureData = friendObject?.optJSONObject("picture")?.optJSONObject("data")
+                val pictureUrl = pictureData?.optString("url")
+                friendList.add(Friend(id, name, pictureUrl))
+            }
+        }
+
+        return friendList
+    }
+
+    data class Friend(
+        val id: String?,
+        val name: String?,
+        val pictureUrl: String?
+    )
+
+    fun getProfileUrl() {
         val accessToken = AccessToken.getCurrentAccessToken()
         val request = GraphRequest.newMeRequest(
             accessToken
         ) { _, response ->
             val jsonObject = response?.jsonObject
-            val id = jsonObject?.getString("id")
-            val name = jsonObject?.getString("name")
-
             val pictureData = jsonObject?.getJSONObject("picture")
             val pictureUrl = pictureData?.getJSONObject("data")?.getString("url")
 
-            println("ID: $id")
-            println("Name: $name")
-            println("Profile Picture URL: $pictureUrl")
+            profileUrl = pictureUrl
         }
-        val parameters  = Bundle()
+        val parameters = Bundle()
         parameters.putString("fields", "id,name,picture.type(large)")
         request.parameters = parameters
         request.executeAsync()
